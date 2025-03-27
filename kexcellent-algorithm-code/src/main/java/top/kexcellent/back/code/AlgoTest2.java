52d401588be4be17565c74af9eb8d011813f7944
kanglele <1186591995@qq.com>
2025-03-27 16:00:23 +0800

    upgrade algorithm

---
+++ kexcellent-algorithm-code/src/main/java/top/kexcellent/back/code/AlgoTest2.java
@@ -0,0 +1,25 @@
+/**
+ * OYO.com Inc.
+ * Copyright (c) 2017-2025 All Rights Reserved.
+ */
+package top.kexcellent.back.code;
+
+import java.util.*;
+
+/**
+ * @author kanglele
+ * @version $Id: AlgoTest2, v 0.1 2025/3/23 09:24 user Exp $
+ */
+public class AlgoTest2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+//        while (in.hasNextInt()) { // 注意 while 处理多个 case
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            System.out.println(a + b);
+//        }
+
+        String s = in.nextLine();
+    }
+}
